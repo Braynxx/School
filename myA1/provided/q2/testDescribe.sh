55d25e5c3098f2a92de77f30800dbaa89313bdf0
Brandt Johner <bjohner@cs.ualberta.ca>
2026-02-06 12:49:43 -0700

    edits to q2

--- myA1/provided/q2/testDescribe.sh
+++ myA1/provided/q2/testDescribe.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 filePath=$1
 printFiles() {
-   if ["$filePath" != *.txt]; then
-      echo "stderr"
+   if [ "$filePath" != *.txt ]; then
+      echo "Error: file must be of *.txt" >&2
       exit 1
    fi
+
    i=1
-   while [$i -le $(wc -l "$filePath")]; do
-      echo "$filePath" head -n "$i"
+   fileText="$(cat "$filePath")"
+   while IFS= read -r innerFile <<< "$fileText"; do
+      echo "Description for test case $innerFile:"
+      echo "$(cat "$innerFile")"
       ((i++))
-   done
+      if [ "$i" -eq 5]; then
+         break
+      fi
+   done <<< "$fileText"
 }
+
 printFiles
 
