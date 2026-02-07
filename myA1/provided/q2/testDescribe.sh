09fc27f056eaf32013bc19dbad1971ee2629cf42
Braynx <braynx@DESKTOP-9M9DJJ5.localdomain>
2026-02-06 18:15:31 -0700

    finished q2

--- myA1/provided/q2/testDescribe.sh
+++ myA1/provided/q2/testDescribe.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 filePath=$1
 printFiles() {
-   if [ "$filePath" != *.txt ]; then
+   if [[ "$filePath" != *.txt ]]; then
       echo "Error: file must be of *.txt" >&2
       exit 1
    fi
 
-   i=1
    fileText="$(cat "$filePath")"
-   while IFS= read -r innerFile <<< "$fileText"; do
-      echo "Description for test case $innerFile:"
-      echo "$(cat "$innerFile")"
-      ((i++))
-      if [ "$i" -eq 5]; then
-         break
-      fi
+   while read -r innerFile; do
+      for word in $innerFile; do
+         echo "Description for test case $word:"
+         if [ -f "$word".desc ]; then
+            echo "$(cat "$word".desc)"
+         else
+            echo "$word No test description"
+         fi
+         done
    done <<< "$fileText"
 }
 
